f4c944963f63def8f062f9a4951619f72b8cfd7f
AzureAD\CaoDinhBa <bacao1994@gmail.com>
2024-06-19 17:27:57 +0700

    stack

---
+++ stack/20ValidParentheses.go
@@ -0,0 +1,28 @@
+package stack
+
+func isValid(s string) bool {
+	var stack []uint8
+
+	for i := 0; i < len(s); i++ {
+		if s[i] == ')' && len(stack) > 0 {
+			if stack[len(stack)-1] != '(' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		} else if s[i] == ']' && len(stack) > 0 {
+			if stack[len(stack)-1] != '[' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		} else if s[i] == '}' && len(stack) > 0 {
+			if stack[len(stack)-1] != '{' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, s[i])
+		}
+	}
+	return len(stack) == 0
+
+}
