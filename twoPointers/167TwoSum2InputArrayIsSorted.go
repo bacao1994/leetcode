97cb11f2edf7e8a00a062f07f13d35fa8b8f9e9c
AzureAD\CaoDinhBa <bacao1994@gmail.com>
2024-06-19 16:55:23 +0700

    2 pointers

---
+++ twoPointers/167TwoSum2InputArrayIsSorted.go
@@ -0,0 +1,18 @@
+package main
+
+func twoSum(numbers []int, target int) []int {
+	left, right := 0, len(numbers)-1
+
+	for left < right {
+		if numbers[left]+numbers[right] > target {
+			right--
+		} else if numbers[left]+numbers[right] < target {
+			left++
+		} else {
+			return []int{left + 1, right + 1}
+		}
+	}
+
+	return []int{}
+
+}
